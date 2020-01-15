b783d1da712eef87d42b1c4c86bb62ea1c1143bc
zhaowen.he <zhaowen.he@datayes.com>
2020-01-15 19:04:20 +0800

    告警模块

---
+++ warning/src/main/java/com/john/alltasks/warning/enums/WarningMethodEnum.java
@@ -0,0 +1,26 @@
+package com.john.alltasks.warning.enums;
+
+/**
+ * author: zhaowen.he
+ * date: 2020/1/15
+ * ticket:
+ * description:
+ */
+public enum WarningMethodEnum {
+    /**
+     * 邮件提醒
+     */
+    EMAIL,
+    /**
+     * 短信提醒
+     */
+    SMS,
+    /**
+     * 微信提醒
+     */
+    WECHAT,
+    /**
+     * 电话提醒
+     */
+    PHONE;
+}
---
+++ warning/src/main/java/com/john/alltasks/warning/wmethod/EmailWarning.java
@@ -0,0 +1,12 @@
+package com.john.alltasks.warning.wmethod;
+
+/**
+ * author: zhaowen.he
+ * date: 2020/1/15
+ * ticket:
+ * description: 邮件提醒
+ */
+public class EmailWarning {
+
+
+}
