4c1bc0bf502b5eef1e51423e65384ccba17a537f
zhaowen.he <zhaowen.he@datayes.com>
2020-01-15 18:27:42 +0800

    初始化

---
+++ warning/src/main/java/com/john/alltasks/warning/WarningApp.java
@@ -0,0 +1,22 @@
+package com.john.alltasks.warning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * author: zhaowen.he
+ * date: 2020/1/15
+ * ticket:
+ * description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
+public class WarningApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WarningApp.class, args);
+    }
+}
